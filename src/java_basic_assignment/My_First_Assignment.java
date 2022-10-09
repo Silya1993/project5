d29ab7a80c8735c7e4b89c81b02b20df1150798c
Silya1993 <silyaak93@gmail.com>
2022-10-08 23:03:57 -0400

    first commit

---
+++ src/java_basic_assignment/My_First_Assignment.java
@@ -0,0 +1,50 @@
+package java_basic_assignment;
+
+public class My_First_Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println(1);
+	System.out.println(2);
+	System.out.println(3);
+	System.out.println(4);
+	System.out.println(5);
+	System.out.println(6);
+	System.out.println(7);
+	System.out.println(8);
+	System.out.println(9);
+	System.out.println(10);
+	System.out.println(11);
+	System.out.println(12);
+	System.out.println(13);
+	System.out.println(14);
+	System.out.println(15);
+	System.out.println(16);
+	System.out.println(17);
+	System.out.println(18);
+	System.out.println(19);
+	System.out.println(20);
+	System.out.print(1);
+	System.out.print(2);
+	System.out.print(3);
+	System.out.print(4);
+	System.out.print(5);
+	System.out.print(6);
+	System.out.print(7);
+	System.out.print(8);
+	System.out.print(9);
+	System.out.print(10);
+	System.out.print(11);
+	System.out.print(12);
+	System.out.print(13);
+	System.out.print(14);
+	System.out.print(15);
+	System.out.print(16);
+	System.out.print(17);
+	System.out.print(18);
+	System.out.print(19);
+	System.out.print(20);
+	}
+
+}
---
+++ src/java_basic_assignment/My_First_Assignment1.java
@@ -0,0 +1,109 @@
+package java_basic_assignment;
+
+public class My_First_Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//int
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		int e = 5;
+		int f = 6;
+		int g = 7;
+		int h = 8;
+		int i = 9;
+		int j =10;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+		
+		
+		//boolean
+		boolean a1 = 1>2;
+		boolean a2 = 2==4;
+		boolean a3 = 6>5;
+		boolean a4 = 9!=8.9;
+		boolean a5 = 8.9<9;
+		boolean a6 = 1.5>1.5;
+		boolean a7 = 3>2;
+		boolean a8 = 9>8;
+		boolean a9 = 1!=2;
+		boolean a10 = 1<1;
+		
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(a7);
+		System.out.println(a8);
+		System.out.println(a9);
+		System.out.println(a10);
+		
+		
+		//char
+		char s1 = 'a';
+		char s2 = 'b';
+		char s3 = 'c';
+		char s4 = 'd';
+		char s5 = 'e';
+		char s6 = 'f';
+		char s7 = 'g';
+		char s8 = 'h';
+		char s9 = 'i';
+		char s10 = 'j';
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(s4);
+		System.out.println(s5);
+		System.out.println(s6);
+		System.out.println(s7);
+		System.out.println(s8);
+		System.out.println(s9);
+		System.out.println(s10);
+		
+		
+		//double
+		double b1 = 1.3;
+		double b2 = 5.4;
+		double b3 = 1.543;
+		double b4 = 3.5;
+		double b5 = 7.1435;
+		double b6 = 2.3;
+		double b7 = 1.8;
+		double b8 = 9.2;
+		double b9 = 5.7;
+		double b10 = 0.0;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+		System.out.println(b5);
+		System.out.println(b6);
+		System.out.println(b7);
+		System.out.println(b8);
+		System.out.println(b9);
+		System.out.println(b10);
+		
+		
+		
+		
+		
+		}
+
+}
